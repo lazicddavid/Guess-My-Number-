e03dba08656e7f362639fc17616c4e360292e2d7
David Lazic <lazic.ddavid@gmail.com>
2025-07-02 19:42:11 +0200

    added text align

--- Guess.js
+++ Guess.js
@@ -6,18 +6,39 @@ const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
 };
 
-document.querySelector(".check").addEventListener(".click", function () {
-  const gues = number(document.querySelector(".guess").value);
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    displayMessage("No number!");
+  } else if (guess === secretNumber) {
+    displayMessage("Correct number!");
+    document.querySelector(".number").textContent = secretNumber;
+    document.body.style.backgroundColor = "green";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "Too high" : "Too low");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("You lost game");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
 });
 
-if (!guess) {
-  displayMessage("No number!");
-} else if (guess === secretNumber) {
-  displayMessage("Correct number!");
-  document.querySelector(".number").textContent = secretNumber;
-  document.body.style.backgroundColor = "green";
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.floor(Math.random() * 20) + 1;
 
-  if (score > highscore) {
-    highscore = score;
-  }
-}
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
+  document.querySelector(".number").textContent = "?";
+  document.body.style.backgroundColor = "#1f1f1f";
+});
